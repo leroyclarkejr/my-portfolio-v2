82839dc455d3c21f5189ded628e6bb6eaac01f69
Leroy Clarke Jr <lclarkejr94@gmail.com>
2020-09-03 01:29:56 -0400

    fixed netlify deployment

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Leroy Clarke Jr. Front-end Developer`,
+    description: `Front-end developer`,
+    author: `@ldcjr`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `Leroy Clarke Jr - Front-end Developer`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#0a70ff`,
+        theme_color: `#0a70ff`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/data/images/ldcjr-logo.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-sass`,
