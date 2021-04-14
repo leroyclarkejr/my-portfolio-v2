87137a4d7af04526ec25e8cba9075b7da41eb4a9
Leroy Clarke Jr <lclarkejr94@gmail.com>
2021-04-13 22:11:49 -0400

    no blog pages being created

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,55 +4,55 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+// // You can delete this file if you're not using it
 
-const path = require(`path`)
-const { createFilePath } = require(`gatsby-source-filesystem`)
+// const path = require(`path`)
+// const { createFilePath } = require(`gatsby-source-filesystem`)
 
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { createPage } = actions
 
-  const blogPost = path.resolve(`./src/templates/blog-template.js`)
-  const result = await graphql(
-    `
-      {
-        allContentfulBlogPost {
-          edges {
-            node {
-              title
-              category
-              slug
-              dateCreated(formatString: "mm/dd/yyyy")
-            }
-          }
-        }
-      }
-    `
-  )
+//   const blogPost = path.resolve(`./src/templates/blog-template.js`)
+//   const result = await graphql(
+//     `
+//       {
+//         allContentfulBlogPost {
+//           edges {
+//             node {
+//               title
+//               category
+//               slug
+//               dateCreated(formatString: "mm/dd/yyyy")
+//             }
+//           }
+//         }
+//       }
+//     `
+//   )
 
-  if (result.errors) {
-    throw result.errors
-  }
+//   if (result.errors) {
+//     throw result.errors
+//   }
 
-  // Create blog posts pages.
-  const posts = result.data.allContentfulBlogPost.edges
+//   // Create blog posts pages.
+//   const posts = result.data.allContentfulBlogPost.edges
 
-  posts.forEach((post, index) => {
-    const previous = index === posts.length - 1 ? null : posts[index + 1].node
-    const next = index === 0 ? null : posts[index - 1].node
+//   posts.forEach((post, index) => {
+//     const previous = index === posts.length - 1 ? null : posts[index + 1].node
+//     const next = index === 0 ? null : posts[index - 1].node
 
-    createPage({
-      path: `blog/${post.node.slug}`,
-      component: blogPost,
-      context: {
-        slug: post.node.slug,
-        category: post.node.category,
-        previous,
-        next,
-      },
-    })
-  })
-}
+//     createPage({
+//       path: `blog/${post.node.slug}`,
+//       component: blogPost,
+//       context: {
+//         slug: post.node.slug,
+//         category: post.node.category,
+//         previous,
+//         next,
+//       },
+//     })
+//   })
+// }
 
 // exports.onCreateNode = ({ node, actions, getNode, createFilePath }) => {
 //   const { createNodeField } = actions
